bc119ad03303ac077ddd52f6c16656db0981c3e6
Elwin Stephan <elwin.stephan@gmail.com>
2020-09-24 22:49:31 +0200

    boolean map

--- week2/The Great Game/main.cpp
+++ week2/The Great Game/main.cpp
@@ -7,7 +7,8 @@ bool holmes_wins(int move_number, int r, int b);
 // 0: Moriarty wins
 // 1: Holmes wins
 // -1: Not set yet
-std::vector<std::vector<std::vector<int>>> dp;
+std::vector<std::vector<std::vector<bool>>> dp;
+std::vector<std::vector<std::vector<bool>>> dp_seen;
 std::vector<std::vector<int>> connections;
 int n;
 
@@ -19,15 +20,15 @@ int n;
 // | 2 | Holmes   | Black |
 // | 3 | Moriarty | Red   |
 // +---+----------+-------+
-
 bool holmes_wins_cache(int move_number, int r, int b) {
 	move_number = move_number % 4;
 
-	if (dp[move_number % 4][r][b] == -1) {
-		dp[move_number % 4][r][b] = holmes_wins(move_number, r, b);
+	if (!dp_seen[move_number][r][b]) {
+		dp[move_number][r][b] = holmes_wins(move_number, r, b);
+		dp_seen[move_number][r][b] = true;
 	}
 
-	return dp[move_number % 4][r][b] == 1;
+	return dp[move_number][r][b];
 }
 
 bool holmes_wins(int move_number, int r, int b) {
@@ -87,23 +88,35 @@ int testcase() {
 		connections[i] = std::vector<int>();
 	}
 
-	dp = std::vector<std::vector<std::vector<int>>>(4);
+	std::cout << "Initialized conns" << std::endl;
+
+	dp = std::vector<std::vector<std::vector<bool>>>(4);
+	dp_seen = std::vector<std::vector<std::vector<bool>>>(4);
 	for (int i = 0; i < 4; ++i) {
-		dp[i] = std::vector<std::vector<int>>(n);
+		dp[i] = std::vector<std::vector<bool>>(n);
+		dp_seen[i] = std::vector<std::vector<bool>>(n);
 		for (int j = 0; j < n; ++j) {
-			dp[i][j] = std::vector<int>(n, -1);
+			dp[i][j] = std::vector<bool>(n);
+			dp_seen[i][j] = std::vector<bool>(n);
 		}
 	}
+
+	std::cout << "Initialized dp" << std::endl;
 
 
 	int r, b; std::cin >> r >> b;
 	r--; b--;
+
+	int maxDiff = 0;
 
 	for (int i = 0; i < m; ++i) {
 		int u, v; std::cin >> u >> v;
 		u--; v--;
 		connections[u].push_back(v);
 	}
+
+	std::cout << "Filled" << std::endl;
+
 	// 0 if Holmes wins, 1 otherwise
 	return !holmes_wins(0, r, b);
 }
