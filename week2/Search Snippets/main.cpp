a06cc02ef2b2c0b57aa6c97b41533c5d9e393f27
Elwin Stephan <elwin.stephan@gmail.com>
2020-09-26 20:15:23 +0200

    search snippets slow

--- week2/Search Snippets/main.cpp
+++ week2/Search Snippets/main.cpp
@@ -3,56 +3,81 @@
 #include <algorithm>
 #include <limits>
 
-struct tuple {
-	int position, word;
+struct identifier {
+	int position, letter;
 };
 
-bool operator < (const tuple& x, const tuple& y) {
+bool operator < (const identifier& x, const identifier& y) {
     return x.position < y.position;
 }
 
-std::vector<tuple> positions;
+std::vector<identifier> positions;
+std::vector<std::vector<int>> letter_sum;
 
-int smallest_snippet(std::vector<int> word_counts, int i, int j) {
-	if (j - i <= word_counts.size()) {
-		return j - i;
+bool valid(std::vector<int> letter_cache) {
+	for (long unsigned int i = 0; i < letter_cache.size(); ++i) {
+		if (letter_cache[i] == 0) {
+			return false;
+		}
 	}
 
-	int left_snippet = std::numeric_limits<int>::max();
-	int right_snippet = std::numeric_limits<int>::max();
-
-	if (word_counts[positions[i].word] > 1) {
-		std::vector<int> left = word_counts; left[positions[i].word]--;
-		left_snippet = smallest_snippet(left, i + 1, j);
-	}
-
-	if (word_counts[positions[j - 1].word] > 1) {
-		std::vector<int> right = word_counts; right[positions[j - 1].word]--;
-		right_snippet = smallest_snippet(right, i, j - 1);
-	}
-
-	return std::min(std::min(left_snippet, right_snippet), j - i);
+	return true;
 }
 
 int testcase() {
 	int n; std::cin >> n;
-	std::vector<int> word_lengths(n);
-	positions = std::vector<tuple>(0);
-	
+	std::vector<int> word_counts(n);
+	positions = std::vector<identifier>(0);
+
 	for (int i = 0; i < n; ++i) {
-		std::cin >> word_lengths[i];
+		std::cin >> word_counts[i];
 	}
 
 	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < word_lengths[i]; ++j) {
+		for (int j = 0; j< word_counts[i]; ++j) {
 			int position; std::cin >> position;
-			positions.push_back(tuple{position, i});
+			positions.push_back(identifier{position, i});
 		}
 	}
 
 	std::sort(positions.begin(), positions.end());
 
-	return smallest_snippet(word_lengths, 0, positions.size());
+	std::vector<int> letter_cache(n, 0);
+	long unsigned int i = 0; long unsigned int j = 0;
+
+	int min_length = std::numeric_limits<int>::max();
+
+	while (j < positions.size()) {
+		// std::cout << "i: " << i << " j: " << j << std::endl;
+		// for (long unsigned int i = 0; i < letter_cache.size(); ++i) {
+		// 	std::cout << letter_cache[i] << " ";
+		// }
+
+		// std::cout << std::endl;
+
+		if (valid(letter_cache)) {		
+			int length = 0;
+			if (j > 0) {
+				length += positions[j - 1].position - positions[i].position + 1;
+			}
+
+			if (length < min_length) {
+				min_length = length;
+			}
+
+			// std::cout << "length: " << length << std::endl;
+
+			// remove first letter
+			letter_cache[positions[i++].letter]--;
+		} else {
+
+			// add next letter
+			letter_cache[positions[j++].letter]++;
+		}
+		
+	}
+
+	return min_length;
 }
 
 int main() {
