5355b9019c46fa1d41854eb4f047c67d9893ddfd
Elwin Stephan <elwin.stephan@gmail.com>
2020-09-26 17:54:49 +0200

    still not working damn matrices

--- week1/Even Matrices/main.cpp
+++ week1/Even Matrices/main.cpp
@@ -1,138 +1,111 @@
 #include <iostream>
 #include <vector>
 
-int testcase() {
+int testcase2() {
 	int n; std::cin >> n;
 
-	// even[i][j] is true iff all entries up and inclusive i, j sum up to an even number
-	std::vector<std::vector<bool>> even(n);
-
-	for (int i = 0; i < n; ++i)
-	{
-		even[i] = std::vector<bool>(n);
-
-		for (int j = 0; j < n; ++j)
-		{
+	// second[i][j] is true iff the sum up to (and inclusive) i, j is even
+	std::vector<std::vector<bool>> second(n);
+	for (int i = 0; i < n; ++i) {
+		second[i] = std::vector<bool>(n);
+		for (int j = 0; j < n; ++j) {
 			int next; std::cin >> next;
-			
+
 			if (i > 0 && j > 0) {
-				even[i][j] = ((next % 2 == 0) + even[i - 1][j - 1] + even[i - 1][j] + even[i][j - 1]) % 2 == 0;
+				second[i][j] = ((next % 2 == 0) + second[i - 1][j - 1] + second[i - 1][j] + second[i][j - 1]) % 2 == 0;
 			} else if (i > 0) {
-				even[i][j] = (next % 2 == 0) == even[i - 1][j];
+				second[i][j] = (next % 2 == 0) == second[i - 1][j];
 			} else if (j > 0) {
-				even[i][j] = (next % 2 == 0) == even[i][j - 1];
+				second[i][j] = (next % 2 == 0) == second[i][j - 1];
 			} else {
-				even[0][0] = next % 2 == 0;
+				second[0][0] = next % 2 == 0;
 			}
 
-			std::cout << even[i][j] << " ";
+			std::cout << second[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	std::cout << "============" << std::endl;
+
+	std::vector<std::vector<int>> third(n);
+	for (int i = 0; i < n; ++i) {
+		third[i] = std::vector<int>(n);
+		for (int j = 0; j < n; ++j) {
+			third[i][j] = second[i][j];
+			if (i > 0) { third[i][j] += third[i - 1][j]; }
+			if (j > 0) { third[i][j] += third[i][j - 1]; }
+			if (i > 0 && j > 0) { third[i][j] -= third[i - 1][j - 1]; }
+
+			std::cout << third[i][j] << " ";
 		}
 
 		std::cout << std::endl;
 	}
 
-	// even_sum[i][j] contains the number of 1s up and inclusive i, j from even
-	std::vector<std::vector<int>> even_sum(n);
+	int singles = 0; int evens = 0; int odds = 0;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
 
-	std::cout << "=============" << std::endl;
-
-	for (int i = 0; i < n; ++i)
-	{
-		even_sum[i] = std::vector<int>(n);
-		for (int j = 0; j < n; ++j)
-		{
-			if (even[i][j]) { even_sum[i][j]++; }
-
-			if (i > 0) { even_sum[i][j] += even_sum[i - 1][j]; }
-			if (j > 0) { even_sum[i][j] += even_sum[i][j - 1]; }
-			if (i > 0 && j > 0) { even_sum[i][j] -= even_sum[i - 1][j - 1]; }
-
-			std::cout << even_sum[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
-
-
-	int singles = 0; int even_pairs = 0; int odd_pairs = 0;
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < n; ++j)
-		{
-			if (even[i][j]) {
+			if (second[i][j]) {
 				singles++;
 
-				even_pairs += even_sum[i][j] - 1;
+				evens += third[i][j] - 1;
 			} else {
-				odd_pairs += (i + 1) * (j + 1) - even_sum[i][j] - 1;
+				odds += (i + 1) * (j + 1) - third[i][j] - 1;
 			}
 		}
 	}
 
-	std::cout << " single: " << singles << "\n pairs even: " << even_pairs << "\n pairs odd: " << odd_pairs << std::endl;
+	std::cout << "singles: " << singles << ", evens: " << evens << ", odds: " << odds << std::endl;
 
-	return singles + even_pairs + odd_pairs;
-	
+	return singles + evens + odds;
+}
 
-	// std::cout << " single: " << evens << "\n pairs even: " << even_pairs[n - 1][n - 1] << "\n pairs odd: " << odd_pairs[n - 1][n - 1] << std::endl;
+int testcase1() {
+	int n; std::cin >> n;
 
-	// return evens + even_pairs[n - 1][n - 1] + odd_pairs[n - 1][n - 1];
+	// second[i][j] is true iff the sum up to (and inclusive) i, j is even
+	std::vector<std::vector<bool>> second(n);
+	for (int i = 0; i < n; ++i) {
+		second[i] = std::vector<bool>(n);
+		for (int j = 0; j < n; ++j) {
+			int next; std::cin >> next;
 
-	// std::cout << "========" << std::endl;
+			if (i > 0 && j > 0) {
+				second[i][j] = ((next % 2 == 0) + second[i - 1][j - 1] + second[i - 1][j] + second[i][j - 1]) % 2 == 0;
+			} else if (i > 0) {
+				second[i][j] = (next % 2 == 0) == second[i - 1][j];
+			} else if (j > 0) {
+				second[i][j] = (next % 2 == 0) == second[i][j - 1];
+			} else {
+				second[0][0] = next % 2 == 0;
+			}
 
-	// for (int i = 0; i < n; ++i) {
-	// 	evencount[i] = std::vector<int>(n);
+			// std::cout << second[i][j] << " ";
+		}
+		// std::cout << std::endl;
+	}
 
-	// 	for (int j = 0; j < n; ++j) {
-	// 		evencount[i][j] = even[i][j];
-	// 		if (i > 0) {
-	// 			evencount[i][j] += evencount[i - 1][j];
-	// 		}
+	int evens = 0;
 
-	// 		if (j > 0) {
-	// 			evencount[i][j] += evencount[i][j - 1];
-	// 		}
+	for (int i0 = 0; i0 < n; ++i0) {
+		for (int j0 = 0; j0 < n; ++j0) {
+			for (int i1 = i0; i1 < n; ++i1) {
+				for (int j1 = j0; j1 < n; ++j1) {
+					if ((i0 != i1 || j0 != j1) && second[i0][j0] == second[i1][j1]) {
+						evens++;
+					}
 
-	// 		if (i > 0 && j > 0) {
-	// 			evencount[i][j] -= evencount[i - 1][j - 1];
-	// 		}
+					if ((i0 == i1 && j0 == j1) && second[i1][j1]) {
+						evens++;
+					}
+				}
+			}
+		}
+	}
 
-	// 		std::cout << evencount[i][j] << " ";
-	// 	}
-	// 	std::cout << std::endl;
-	// }
-
-	// int single_evens = 0;
-	// int pair_even = 0;
-	// int pair_odd = 0;
-
-	// // even[i][j] contains how many evens are contained in the upper left corner
-	// std::vector<std::vector<int>> evencount(n);
-
-	// for (int i = 0; i < n; ++i) {
-	// 	for (int j = 0; j < n; ++j) {
-	// 		if (even[i][j]) {
-	// 			single_evens++;
-
-	// 			if (i > 0) {
-	// 				pair_even += evencount[i - 1][j];
-	// 			}
-
-	// 			if (j > 0) {
-	// 				pair_even += evencount[i][j - 1];
-	// 			}
-
-	// 			if (i > 0 && j > 0) {
-	// 				pair_even += evencount[i - 1][j - 1] - 1;
-	// 			}
-	// 		} else {
-	// 			pair_odd += (i + 1) * (j + 1) - evencount[i][j] - 1;
-	// 		}
-	// 	}
-	// }
-
-	// std::cout << " single: " << single_evens << "\n pairs even: " << pair_even << "\n pairs odd: " << pair_odd << std::endl;
-
-	// return single_evens + pair_even + pair_odd;
+	return evens;
 }
 
 
@@ -142,7 +115,7 @@ int main() {
 	int t; std::cin >> t;
 
 	for (int i = 0; i < t; ++i) {
-		int result = testcase();
+		int result = testcase1();
 		std::cout << result << "\n";
 	}
 
