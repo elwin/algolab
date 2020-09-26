6c97dd9cc9d7fa4abba2bf73ce5d3c7f52578576
Elwin Stephan <elwin.stephan@gmail.com>
2020-09-26 23:39:34 +0200

    yses for search snippets

--- week2/Search Snippets/main.cpp
+++ week2/Search Snippets/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <limits>
+#include <set>
 
 struct identifier {
 	int position, letter;
@@ -12,16 +13,11 @@ bool operator < (const identifier& x, const identifier& y) {
 }
 
 std::vector<identifier> positions;
-std::vector<std::vector<int>> letter_sum;
+std::vector<int> letter_cache;
+std::set<int> missing;
 
-bool valid(std::vector<int> letter_cache) {
-	for (long unsigned int i = 0; i < letter_cache.size(); ++i) {
-		if (letter_cache[i] == 0) {
-			return false;
-		}
-	}
-
-	return true;
+bool valid() {
+	return missing.size() == 0;
 }
 
 int testcase() {
@@ -42,7 +38,10 @@ int testcase() {
 
 	std::sort(positions.begin(), positions.end());
 
-	std::vector<int> letter_cache(n, 0);
+	letter_cache = std::vector<int>(n, 0);
+	missing = std::set<int>();
+	for (int i = 0; i < n; ++i) { missing.insert(i); }
+
 	long unsigned int i = 0; long unsigned int j = 0;
 
 	int min_length = std::numeric_limits<int>::max();
@@ -59,7 +58,7 @@ int testcase() {
 			return min_length;
 		}
 
-		if (valid(letter_cache)) {		
+		if (valid()) {
 			int length = 0;
 			if (j > 0) {
 				length += positions[j - 1].position - positions[i].position + 1;
@@ -72,11 +71,21 @@ int testcase() {
 			// std::cout << "length: " << length << std::endl;
 
 			// remove first letter
-			letter_cache[positions[i++].letter]--;
-		} else {
+			int letter = positions[i++].letter;
+			letter_cache[letter]--;
 
+			if (letter_cache[letter] == 0) {
+				missing.insert(letter);
+			}
+		} else {
 			// add next letter
-			letter_cache[positions[j++].letter]++;
+			int letter = positions[j++].letter;
+			letter_cache[letter]++;
+			
+			// freshly appeared
+			if (letter_cache[letter] == 1) {
+				missing.erase(missing.find(letter));
+			}
 		}
 		
 	}
