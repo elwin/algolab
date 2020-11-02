84c3c6bc434abd061e67fef14375baf282f23d41
Elwin Stephan <elwin.stephan@gmail.com>
2020-11-02 15:09:06 +0100

    some progerss with antenna

---
+++ week3/Antenna/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <CGAL/Exact_predicates_exact_constructions_kernel_with_sqrt.h>
+#include <CGAL/Min_circle_2.h>
+#include <CGAL/Min_circle_2_traits_2.h>
+#include <CGAL/number_utils.h>
+
+
+typedef CGAL::Exact_predicates_exact_constructions_kernel_with_sqrt K;
+typedef CGAL::Min_circle_2_traits_2<K>                              Traits;
+typedef CGAL::Min_circle_2<Traits>                                  Min_circle;
+
+double ceil_to_double(const K::FT& x) {
+	double a = std::ceil(CGAL::to_double(x));
+	while (a > x) a--;
+	while (a < x) a++;
+
+	return a;
+}
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+
+	while (true) {
+		int n; std::cin >> n;
+		if (n == 0) {
+			return 0;
+		}
+
+		std::vector<K::Point_2> points(n);
+		for (int i = 0; i < n; ++i) {
+			long x, y; std::cin >> x >> y;
+			points[i] = K::Point_2(x, y);
+		}
+
+		Min_circle circle(points.begin(), points.end(), true);
+
+		auto radius = CGAL::sqrt(circle.circle().squared_radius());
+
+		std::cout << ceil_to_double(radius) << std::endl;
+	}
+}
