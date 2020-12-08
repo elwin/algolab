b17327677c7e9b52e99b16906d78cb38c208f58a
Elwin Stephan <elwin.stephan@gmail.com>
2020-12-08 21:00:24 +0100

    did something

--- week8/Clues/main.cpp
+++ week8/Clues/main.cpp
@@ -10,8 +10,14 @@ struct Point {
 	int x, y;    
 };
 
+struct Status {
+	bool visited;
+	bool first;
+	int component;
+};
+
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Triangulation_vertex_base_with_info_2<int, K> Vb;
+typedef CGAL::Triangulation_vertex_base_with_info_2<Status, K> Vb;
 typedef CGAL::Triangulation_face_base_2<K> Fb;
 typedef CGAL::Triangulation_data_structure_2<Vb,Fb> Tds;
 typedef CGAL::Delaunay_triangulation_2<K, Tds> Triangulation;
@@ -51,7 +57,7 @@ void testcase(bool debug) {
 	Triangulation t, t0, t1;
 	t.insert(stations.begin(), stations.end());
 	for (Vertex_iterator v = t.finite_vertices_begin(); v != t.finite_vertices_end(); v++) {
-		v->info() = -1;
+		v->info() = Status{false, false, -1};
 	}
 
 	bool possible = true;
