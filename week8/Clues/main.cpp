4594d02f8bfd3875d8e842f7caf8e0ba2d193ac3
Elwin Stephan <elwin.stephan@gmail.com>
2020-12-10 11:08:22 +0100

    passing some test sets

--- week8/Clues/main.cpp
+++ week8/Clues/main.cpp
@@ -4,7 +4,12 @@
 #include <CGAL/Delaunay_triangulation_2.h>
 #include <CGAL/Triangulation_vertex_base_with_info_2.h>
 
-#include <boost/graph/adjacency_list.hpp>
+void ass(bool assertion, std::string reason) {
+	if (!assertion) {
+		std::cout << "Something went wrong: " << reason << std::endl;
+		exit(-1);
+	}
+}
 
 struct Point {
 	int x, y;    
@@ -26,6 +31,8 @@ typedef Triangulation::Finite_faces_iterator Face_iterator;
 typedef Triangulation::Vertex_iterator Vertex_iterator;
 typedef Triangulation::Edge_iterator Edge_iterator;
 typedef Triangulation::Vertex_handle Vertex_handle;
+typedef Triangulation::Edge_circulator Edge_circulator;
+typedef Triangulation::Vertex_circulator Vertex_circulator;
 
 int squared_distance(Point a, Point b) {
 	return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
@@ -55,43 +62,86 @@ void testcase(bool debug) {
 	Triangulation t, t0, t1;
 	t.insert(stations.begin(), stations.end());
 	for (Vertex_iterator v = t.finite_vertices_begin(); v != t.finite_vertices_end(); v++) {
-		v->info() = Status{false, false, false, -1};
+		v->info() = Status{false, false, false, 0};
+	}
+
+	// Figure out components
+	int current_component = 1;
+	for (Vertex_iterator v = t.finite_vertices_begin(); v != t.finite_vertices_end(); v++) {
+		if (v->info().visited_component) continue;
+
+		std::stack<Vertex_handle> stack;
+		stack.push(v);
+
+		while (!stack.empty()) {
+			Vertex_handle next = stack.top(); stack.pop();
+			if (next->info().visited_component) continue;
+
+			next->info().component = current_component;
+			next->info().visited_component = true;
+
+			Vertex_circulator c = t.incident_vertices(next);
+			do {
+				if (t.is_infinite(c) || CGAL::squared_distance(c->point(), next->point()) > r * r) { continue; }
+				stack.push(c);
+			} while (++c != t.incident_vertices(next));
+		}
+
+		current_component++;
 	}
 
 	bool possible = true;
-	for (Edge_iterator e = t.finite_edges_begin(); e != t.finite_edges_end(); e++) {
-		auto a = e->first->vertex((e->second + 1) % 3);
-		auto b = e->first->vertex((e->second + 2) % 3);
+	for (Vertex_iterator v = t.finite_vertices_begin(); v != t.finite_vertices_end(); v++) {
+		std::stack<Vertex_handle> stack1;
+		std::stack<Vertex_handle> stack2;
 
-		if (!a->info().visited && b->info().visited) {
-			a->info().visited = true; a->info().first = true;
-			b->info().visited = true; b->info().first = false;
-		} else if (a->info().first == b->info().first) {
-			if (t.segment(e).squared_length() > r * r) {
-				continue;
+		if (v->info().visited) continue;
+		stack1.push(v);
+
+		while (!stack1.empty() || !stack2.empty()) {
+			while (!stack1.empty()) {
+				Vertex_handle next = stack1.top(); stack1.pop();
+				if (next->info().visited) {
+					if (next->info().first != true)
+						possible = false;
+
+					continue;
+				}
+
+				next->info().visited = true;
+				next->info().first = true;
+
+				Vertex_circulator c = t.incident_vertices(next);
+				do {
+					if (t.is_infinite(c) || CGAL::squared_distance(c->point(), next->point()) > r * r) { continue; }
+					stack2.push(c);
+				} while (++c != t.incident_vertices(next));
 			}
 
-			possible = false;
-			break;
-		} else {
-			if (!a->info().visited) {
-				auto temp = a;
-				a = b;
-				b = temp;
-			}
+			while (!stack2.empty()) {
+				Vertex_handle next = stack2.top(); stack2.pop();
+				if (next->info().visited) {
+					if (next->info().first != false)
+						possible = false;
 
-			b->info().first = !a->info().first;
-			b->info().visited = true;
+					continue;
+				}
+				next->info().visited = true;
+				next->info().first = false;
+
+				Vertex_circulator c = t.incident_vertices(next);
+				do {
+					if (t.is_infinite(c) || CGAL::squared_distance(c->point(), next->point()) > r * r) { continue; }
+					stack1.push(c);
+				} while (++c != t.incident_vertices(next));
+			}
 		}
 	}
 
-	if (!possible) {
-		for (int i = 0; i < m; ++i) {
-			std::cout << 'x';
-		}
-		std::cout << std::endl;
-
-		return;
+	for (Vertex_iterator v = t.finite_vertices_begin(); v != t.finite_vertices_end(); v++) {
+		ass(v->info().visited == true, "not all vertices visited");
+		ass(v->info().visited_component == true, "not all vertices visited");
+		ass(v->info().component > 0, "not all components larger than 0");
 	}
 
 	for (Vertex_iterator v = t.finite_vertices_begin(); v != t.finite_vertices_end(); v++) {
@@ -104,6 +154,17 @@ void testcase(bool debug) {
 		} else {
 			t1.insert(v->point());
 		}
+	}
+
+	if (debug) {
+		int count1 = 0; int count2 = 0;
+		for (Edge_iterator e = t0.finite_edges_begin(); e != t0.finite_edges_end(); e++) {
+			count1++;
+		}
+		for (Edge_iterator e = t1.finite_edges_begin(); e != t1.finite_edges_end(); e++) {
+			count2++;
+		}
+		std::cout << "assignment count: " << count1 << " / " << count2 << std::endl;
 	}
 
 	for (Edge_iterator e = t0.finite_edges_begin(); e != t0.finite_edges_end(); e++) {
@@ -130,26 +191,6 @@ void testcase(bool debug) {
 		return;
 	}
 
-	// Figure out components
-	for (Vertex_iterator v = t.finite_vertices_begin(); v != t.finite_vertices_end(); v++) {
-		std::stack<Vertex_handle> stack;
-		stack.push(v);
-
-		int current_component = 0;
-		while (!stack.empty()) {
-			Vertex_handle next = stack.top(); stack.pop();
-			if (next->info().visited_component) {
-				continue;
-			}
-
-			next->info().component = current_component;
-			next->info().visited_component = true;
-		}
-
-		current_component++;
-	}
-
-
 	for (int i = 0; i < m; ++i) {
 		Point a = clues_a[i];
 		Point b = clues_b[i];
@@ -157,7 +198,10 @@ void testcase(bool debug) {
 		Vertex_handle a_handle = t.nearest_vertex(K::Point_2(a.x, a.y));
 		Vertex_handle b_handle = t.nearest_vertex(K::Point_2(b.x, b.y));
 
-		if (a_handle->info().component == b_handle->info().component) {
+		if (CGAL::squared_distance(K::Point_2(a.x, a.y), K::Point_2(b.x, b.y)) <= r * r || (
+			CGAL::squared_distance(a_handle->point(), K::Point_2(a.x, a.y)) <= r * r &&
+			CGAL::squared_distance(b_handle->point(), K::Point_2(b.x, b.y)) <= r * r &&
+			a_handle->info().component == b_handle->info().component)) {
 			std::cout << 'y';
 		} else {
 			std::cout << 'n';
