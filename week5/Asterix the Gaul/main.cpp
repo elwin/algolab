b06377a5a11094eb815c5b53bb8849e43b67c81c
Elwin Stephan <elwin.stephan@gmail.com>
2020-12-25 17:56:47 +0100

    pass test3

--- week5/Asterix the Gaul/main.cpp
+++ week5/Asterix the Gaul/main.cpp
@@ -20,6 +20,26 @@ Num max_distance(std::vector<movement> &movements, int i, Num remainingTime, Num
 	return max;
 }
 
+int search(std::vector<movement> &movements, std::vector<Num> &sips, Num D, Num T, size_t l, size_t u) {
+	if (sips.size() == 0) return -1;
+	if (u == l) return max_distance(movements, movements.size() - 1, T, sips[l]) >= D ? l : -1;
+
+	if (u - 1 == l) {
+		if (search(movements, sips, D, T, l, l) != -1) return l;
+		if (u < sips.size() && search(movements, sips, D, T, u, u) != -1) return u;
+		return -1;
+	}
+
+	size_t middle = (l + u - 1) / 2;
+	Num cur = max_distance(movements, movements.size() - 1, T, sips[middle]);
+
+	if (cur < D) {
+		return search(movements, sips, D, T, middle + 1, u);
+	}
+
+	return search(movements, sips, D, T, l, middle + 1);
+}
+
 
 int testcase() {
 	size_t n, m; Num D, T; std::cin >> n >> m >> D >> T;
@@ -38,13 +58,15 @@ int testcase() {
 		return 0;
 	}
 
-	for (size_t i = 0; i < m; ++i) {
-		if (max_distance(movements, movements.size() - 1, T, sips[i]) >= D) {
-			return i + 1;
-		}
-	}
+	// for (size_t i = 0; i < m; ++i) {
+	// 	if (max_distance(movements, movements.size() - 1, T, sips[i]) >= D) {
+	// 		return i + 1;
+	// 	}
+	// }
 
-	return -1;
+	int i = search(movements, sips, D, T, 0, sips.size());
+
+	return i > -1 ? i + 1 : -1;
 }
 
 
