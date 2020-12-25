51e35ca5bf4511892ea695664c96a208a5fc18b8
Elwin Stephan <elwin.stephan@gmail.com>
2020-12-25 16:55:19 +0100

    pass slow testcases

--- week5/Asterix the Gaul/main.cpp
+++ week5/Asterix the Gaul/main.cpp
@@ -1,64 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <limits>
 
 typedef long long Num;
 
 struct movement {
-	// TODO: d / t can be 2^50
 	Num d, t; 
 };
 
-bool greater(movement a, movement b) {
-	// std::cout << a.d << " " << a.t << " - " << a.d << " " << a.t << std::endl;
+Num max_distance(std::vector<movement> &movements, int i, Num remainingTime, Num sip) {
+	if (i == -1) return 0;
 
-	if (a.d * b.t == b.d * a.t) {
-		return a.d > b.d;
+	Num max = max_distance(movements, i - 1, remainingTime, sip);
+	if (movements[i].t < remainingTime) {
+		max = std::max(max, max_distance(movements, i - 1, remainingTime - movements[i].t, sip) + movements[i].d + sip);
 	}
 
-	return a.d * b.t > b.d * a.t;
+	return max;
 }
-
-// bool greater(movement a, movement b) {
-// 	return !less(a, b);
-// }
 
 
 int testcase() {
-	// TODO: D / T can be 2^50
 	size_t n, m; Num D, T; std::cin >> n >> m >> D >> T;
 	
 	std::vector<movement> movements(n);
 	for (size_t i = 0; i < n; ++i) {
 		std::cin >> movements[i].d >> movements[i].t;
 	}
-	// TODO: s can be 2^50
+
 	std::vector<Num> sips(m);
 	for (size_t i = 0; i < m; ++i) {
 		std::cin >> sips[i];
 	}
 
-	std::sort(movements.begin(), movements.end(), greater);
-	// for (size_t i = 0; i < n; ++i) {
-	// 	std::cout << movements[i].d << " " << movements[i].t << std::endl;
-	// }
-
-	for (size_t i = 0; i < n; ++i) {
-		// std::cout << D << " " << T << std::endl;
-		if (D <= 0) return 0;
-		if (movements[i].t >= T) continue;
-		T -= movements[i].t;
-		D -= movements[i].d;
+	if (max_distance(movements, movements.size() - 1, T, 0) >= D) {
+		return 0;
 	}
-		// std::cout << D << " " << T << std::endl;
 
-
-	if (D <= 0) return 0;
-
-	int current = 0;
 	for (size_t i = 0; i < m; ++i) {
-		current += sips[i];
-		if (current >= D) return i + 1;
+		if (max_distance(movements, movements.size() - 1, T, sips[i]) >= D) {
+			return i + 1;
+		}
 	}
 
 	return -1;
@@ -66,7 +49,6 @@ int testcase() {
 
 
 int main() {
-
 	size_t t; std::cin >> t;
 	while (t--) {
 		int out = testcase();
