9f722722ffd1034fcfdf34dac17cbd907ac6053e
Elwin Stephan <elwin.stephan@gmail.com>
2021-02-02 11:57:15 +0100

    solve all, but slow

--- problems/Punch/main.cpp
+++ problems/Punch/main.cpp
@@ -1,34 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <limits>
 
 // <cost, volume>
 typedef std::vector<std::pair<size_t, size_t>> D;
-typedef std::vector<std::vector<int>> DP;
 
-int min_cost(D drinks, size_t i, size_t cur, size_t k, DP &dp);
+// <cost, variation>
+typedef std::pair<int, int> Res;
+typedef std::vector<std::vector<Res>> DP;
 
-int min_cost_dp(D drinks, size_t i, size_t cur, size_t k, DP &dp) {
-	if (cur > k) return min_cost(drinks, i, cur, k, dp);
-	if (dp[i][cur] == 0) {
+Res min_cost(D &drinks, size_t i, size_t cur, size_t k, DP &dp);
+
+Res min_cost_dp(D &drinks, size_t i, size_t cur, size_t k, DP &dp) {
+	if (cur >= k) return min_cost(drinks, i, cur, k, dp);
+	if (dp[i][cur].first == 0) {
 		dp[i][cur] = min_cost(drinks, i, cur, k, dp);
 	}
 
 	return dp[i][cur];
 }
 
-int min_cost(D drinks, size_t i, size_t cur, size_t k, DP &dp) {
-	if (cur >= k) return 0;
-	if (i == drinks.size()) return -1;
+Res min_cost(D &drinks, size_t i, size_t cur, size_t k, DP &dp) {
+	if (cur >= k) return {0, 0};
+	if (i == drinks.size()) return {-1, 0};
 
 	auto d = drinks[i];
-	int min = 10000;
+	Res min = {std::numeric_limits<int>::max(), 0};
 	for (size_t j = 0; ; ++j) {
-		// std::cout << i << " " << j << std::endl;
-
-		int next = min_cost_dp(drinks, i + 1, cur + d.second * j, k, dp);
-		if (next == -1) continue;
+		Res next = min_cost_dp(drinks, i + 1, cur + d.second * j, k, dp);
+		if (next.first == -1) continue;
 		
-		min = std::min(min, next + int(d.first * j));
+		next.first += int(d.first * j);
+		if (j > 0) next.second--;
+
+		min = std::min(min, next);
 
 		if (cur + d.second * j > k) break;
 	}
@@ -43,12 +48,17 @@ void testcase() {
 		std::cin >> drinks[i].first >> drinks[i].second;
 	}
 
-	DP dp = DP(n + 1, std::vector<int>(k + 1));
+	DP dp = DP(n + 1, std::vector<Res>(k + 1));
 
-	std::cout << min_cost_dp(drinks, 0, 0, k, dp) << std::endl;
+	Res r = min_cost_dp(drinks, 0, 0, k, dp);
+
+	std::cout << r.first << " " << -r.second << std::endl;
 }
 
 int main() {
+	std::ios_base::sync_with_stdio(false);
 	size_t t; std::cin >> t;
-	while (t--) testcase();
+	while (t--) {
+		testcase();
+	}
 }
